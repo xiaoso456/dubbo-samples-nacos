0e5245388ade2e11d3e41581cceaea044f0fd676
xiaoso <xiaoso456@qq.com>
2023-03-05 20:21:44 +0800

    first commit

---
+++ basic/dubbo-samples-api/src/main/java/com/github/xiaoso456/provider/Application.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.xiaoso456.provider;
+
+import com.github.xiaoso456.api.GreetingsService;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // 服务配置
+        ServiceConfig<GreetingsService> service = new ServiceConfig<>();
+        service.setInterface(GreetingsService.class);
+        service.setRef(new GreetingsServiceImpl());
+
+        // 启动服务提供者实例
+        DubboBootstrap.getInstance()
+                .application("first-dubbo-provider")
+                // 这里是注册中心地址
+                .registry(new RegistryConfig("nacos://localhost:8848"))
+                .protocol(new ProtocolConfig("dubbo", -1))
+                .service(service)
+                .start()
+                .await();
+    }
+}
